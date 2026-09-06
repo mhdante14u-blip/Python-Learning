7948925cdc275a364e25be760382d304f42ed698
mh_Dante4u <mhdante14u@gmail.com>
2026-09-06 15:11:27 +0600

    Add files via upload

---
+++ Input fucntion.py
@@ -0,0 +1,3 @@
+
+# input is a function which takes the command from a user , like a prompt and takes and keeps it whatever the prompt given by the user and in order to see the print() function is used.
+print("Hello " + input("What is your name?") + "!")
