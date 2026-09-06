01374f054e03e6a61f4bc244736862dc9d0dec0a
mh_Dante4u <mhdante14u@gmail.com>
2026-09-06 12:06:58 +0600

    Add files via upload

---
+++ 2.String concatenation and n working process.py
@@ -0,0 +1,35 @@
+# Write your code below this  line 👇
+# String concatenation and \n working process
+# String concatenation method
+
+# 3 ways do make space in the code and + sign is for to connect or concatenation
+# 1st way :
+# After writing one sentence or a word of line end of double quotation mark put a space, for the multiple sentences just add the space at end of the word or the sentences not for the last sentence and connect or concatenation with + sign and write a new sentence or word but all has to be in one same line inside using  the one parentheses()
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water is bowl. " + "2. knead the dough for 10 minutes. " + "3. Add 3g of Salt. " + "4. Leave to rise for 2 hours. " + "5. Bake at 200 degrees C for 30 minutes. ")
+
+# 2nd way :
+
+# After writing one sentence or a word of line end of double quotation mark do not put a or any space instead use + sign to connect or concatenation a new sentence or word and while writing inside the opening double quotation mark put a space and then write the new sentence or the word but all has to be in one same line inside using the one parentheses()
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water is bowl." + " 2. knead the dough for 10 minutes." + " 3. Add 3g of Salt." + " 4. Leave to rise for 2 hours." + " 5. Bake at 200 degrees C for 30 minutes.")
+
+# 3rd way :
+
+#  After writing one sentence or a word Connect or concatenation use + sign then put double quotation mark and put space inside double quotation then use + sign to join another new word or sentence but all has to be in the same line inside using the one parentheses()
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water is bowl." + " " + "2. knead the dough for 10 minutes." + " " + "3. Add 3g of Salt." + " " +"4. Leave to rise for 2 hours." + " " + "5. Bake at 200 degrees C for 30 minutes.")
+
+# bonus tip :
+
+# After writing one sentence or a word before writing a new sentence or a word Also putting a comma or , sign instead of + sign do the connect or concatenation inside a print function but all has to be in the same line inside using the one parentheses()
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water is bowl." , "2. knead the dough for 10 minutes." , "3. Add 3g of Salt." , "4. Leave to rise for 2 hours." , "5. Bake at 200 degrees C for 30 minutes.")
+
+# \n working method
+
+# Multiple words or sentences from one print statement line to get next or separate then between writing a complete statement put \n inside that print function but all has to be in the same line using the one parentheses()
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water is bowl.\n2. knead the dough for 10 minutes.\n3. Add 3g of Salt.\n4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.")
+
+
